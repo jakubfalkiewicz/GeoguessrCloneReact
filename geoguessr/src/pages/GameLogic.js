86a5d4f3fc3cd4804cae0f0803b7bb15c06bb149
Jakub Falkiewicz <kufal222@gmail.com>
2022-12-24 04:42:40 +0100

    move pan zoom setup switches impact game

--- geoguessr/src/pages/GameLogic.js
+++ geoguessr/src/pages/GameLogic.js
@@ -19,6 +19,12 @@ const GameLogic = ({ games }) => {
       .childNodes[0];
     panorama.style.width = "100%";
     panorama.style.height = "100vh";
+    if (!game.pan) {
+      panorama.addEventListener("click", () => {
+        panorama.style.pointerEvents = "none";
+      });
+      panorama.click();
+    }
   });
 
   const location = game.locations[round];
@@ -28,13 +34,67 @@ const GameLogic = ({ games }) => {
     lng: location.lng,
   };
 
-  const options = {};
+  const options = {
+    //COMPASS
+    panControl: true,
+    // panControlOptions: {
+    //   position: google.maps.ControlPosition.LEFT_BOTTOM,
+    // },
+    draggable: game.pan,
+    zoomControl: game.zoom,
+    //ROAD ARROWS
+    linksControl: game.move,
+    //ZOOM
+    scrollwheel: game.zoom,
+    //MOVING
+    clickToGo: game.move,
+    //SHOW ADDRESS
+    addressControl: false,
+    //ROAD NAMES
+    showRoadLabels: false,
+    //ADDONS
+    fullscreenControl: false,
+    motionTrackingControl: true,
+    enableCloseButton: false,
+    visible: true,
+  };
+  if (!game.move) {
+    window.addEventListener(
+      "keydown",
+      (event) => {
+        if (
+          // Change or remove this condition depending on your requirements.
+          (event.key === "ArrowUp" || // Move forward
+            event.key === "ArrowDown" || // Move forward
+            event.key === "ArrowLeft" || // Pan left
+            event.key === "ArrowRight") && // Pan right // Zoom out
+          !event.metaKey &&
+          !event.altKey &&
+          !event.ctrlKey
+        ) {
+          event.stopPropagation();
+        }
+      },
+      { capture: true }
+    );
+  }
 
   return (
     <div className="panorama-container">
       <GoogleMap center={center} zoom={10}>
-        <StreetViewPanorama id="street-view" position={center} visible={true} />
+        <StreetViewPanorama
+          id="street-view"
+          position={center}
+          visible={true}
+          options={options}
+        />
       </GoogleMap>
+      <div id="startingPoint" aria-label="Starting point">
+        <img
+          src="https://www.geoguessr.com/_next/static/images/icon-return-to-start-3b4eed3225adfd860a4ed3726ad1e05a.svg"
+          alt="backToStart"
+        />
+      </div>
     </div>
   );
 };
